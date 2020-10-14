cff3801d920446f3853192754bf7ca7d1a132770
Ningyuan Pan <pan.neo@hotmail.com>
2020-10-14 18:49:28 +0800

    Add the support for eureka cluster

--- src/main/java/ningyuan/pan/spring/boot/servicex/web/controller/XServiceController.java
+++ src/main/java/ningyuan/pan/spring/boot/servicex/web/controller/XServiceController.java
@@ -5,6 +5,7 @@ package ningyuan.pan.spring.boot.servicex.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +22,13 @@ public class XServiceController {
 	
 	@Autowired
 	@Qualifier(value="xserviceimpl")
-	XService xservice;
+	private XService xservice;
+	
+	@Value("${server.port}")
+	private String port;
 	
 	@GetMapping(value="/name")
 	public String getName() {
-		return xservice.getName();
+		return xservice.getName() + " from port: "+port;
 	}
 }
